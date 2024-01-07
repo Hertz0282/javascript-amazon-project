4100ffc8cbefe60f450ca2cec7209d5f44878ec1
Piyush <piyushbehera135@gmail.com>
2024-01-07 20:54:52 +0530

    use modules and import cart from cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,3 @@
-const cart = []
+// we will use module to prevent naming error in different scripts.This also helps in running only 1 script at every time.(So no running 1 or more scripts simultaneosly ) 
+
+export const cart = [];
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 // here we are using an array to show the products and as a product has different paramaters to it we shall be using object inside the array to represent these products. Below is how we represent html in javascript
+import { cart } from "../data/cart.js";
 
 let productsHTML = "";
 
