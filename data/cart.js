0eab2eafbd0455a3f14d154a34564a99ba43b592
Piyush <piyushbehera135@gmail.com>
2024-01-09 20:48:35 +0530

    delete product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 // we will use module to prevent naming error in different scripts.This also helps in running only 1 script at every time.(So no running 1 or more scripts simultaneosly ) 
 
-export const cart = [{
+export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
 }, {
@@ -26,4 +26,16 @@ export function addToCart(productId, productName) {
             quantity: 1,
         }); 
     }
+};
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
 }
